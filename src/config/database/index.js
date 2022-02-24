538063056a01bb6ee0041ba17ab05735f8355f94
rochafialvin <rochafi.dev@gmail.com>
2022-02-24 15:12:31 +0700

    Update configuration for database

--- src/config/database/index.js
+++ src/config/database/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 const { DB_USER, DB_NAME, DB_PASS } = proces.env;
 
-const mysql2 = mysql.createConnection({
+const mysql2 = mysql.createPool({
   host: "localhost",
   user: DB_USER,
   password: DB_PASS,
