7a003ed9e9a0eddb16e98a090afeff6fb9797538
rizky2330 <rizky.lazuardi23@gmail.com>
2022-03-01 15:36:29 +0700

    delete unused codes

--- src/routers/attandances/get.attandances.js
+++ src/routers/attandances/get.attandances.js
@@ -5,7 +5,7 @@ router.get("/:userId", async (req, res, next) => {
     const connection = await pool.promise().getConnection();
 
     const sqlGetAttandances =
-      "SELECT  attandances.userId, attandances.id, attandances.tanggal, attandances.checkIn, attandances.checkOut, attandances.status, users.fullName, users.email FROM attandances JOIN users ON users.id=attandances.userId WHERE userId = ?;";
+      "SELECT  attandances.userId, attandances.id, attandances.createdAt, attandances.checkIn, attandances.checkOut, attandances.status, users.fullName, users.email FROM attandances JOIN users ON users.id=attandances.userId WHERE userId = ?;";
     const dataGetAttendances = req.params.userId;
 
     const result = await connection.query(
@@ -13,7 +13,7 @@ router.get("/:userId", async (req, res, next) => {
       dataGetAttendances
     );
     connection.release();
-    console.log(req.body);
+
     res.status(200).send(result);
   } catch (error) {
     next(error);
